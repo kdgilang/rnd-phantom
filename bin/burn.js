e3ded432a7e146690431f57a967c5f4d02c70abd
Gilang Putra <gilang.putra@ogilvy.com.au>
2017-07-24 13:11:36 +0800

    PEFORMANCE IMPROVEMENT

--- bin/burn.js
+++ bin/burn.js
@@ -2,14 +2,53 @@
 
 // global variable
 const printer = require('../lib/printer.js')
+const zipper = require('../lib/zipper.js')
+const GIFEncoder = require('gifencoder')
+//const Canvas = require('canvas')
+const fs = require('fs')
+const argv = require('minimist')(process.argv.slice(2));
 
 // initial 
 function init() {
-	process.argv.forEach(function (val, index, array) {
-		delay = (array[2] !== undefined) ? array[2] : 30000
-		folders = (array[3] !== undefined) ? array[3] : undefined
-		iszipper = (array[4] !== undefined) ? array[4] : "true"
-		selector = (array[5] !== undefined) ? array[5] : "#banner" 
-	})
-	printer.init(delay, folders, selector)
+	delay = (argv.d !== undefined) ? argv.d : (argv.delay !== undefined) ? argv.delay : 30000
+	folder = (argv.f !== undefined) ? argv.folder : (argv.folder !== undefined) ? argv.folder : undefined
+	iszip = (argv.z !== undefined) ? argv.z : (argv.zip !== undefined) ? argv.zip : true
+	iscapture = (argv.c !== undefined) ? argv.c : (argv.capture !== undefined) ? argv.capture : true
+	selector = (argv.s !== undefined) ? argv.s : (argv.selector !== undefined) ? argv.selector : "#banner"
+
+	if(iscapture === true) {
+		printer.init(delay, folder, selector, iszip)
+	} else {
+		zipper.init(folder)
+	}
+
+	// var encoder = new GIFEncoder(320, 240);
+	// // stream the results as they are available into myanimated.gif
+	// encoder.createReadStream().pipe(fs.createWriteStream('myanimated.gif'));
+
+	// encoder.start();
+	// encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+	// encoder.setDelay(500);  // frame delay in ms
+	// encoder.setQuality(10); // image quality. 10 is default.
+
+	// // use node-canvas
+	// var canvas = new Canvas(320, 240);
+	// var ctx = canvas.getContext('2d');
+
+	// // red rectangle
+	// ctx.fillStyle = '#ff0000';
+	// ctx.fillRect(0, 0, 320, 240);
+	// encoder.addFrame(ctx);
+
+	// // green rectangle
+	// ctx.fillStyle = '#00ff00';
+	// ctx.fillRect(0, 0, 320, 240);
+	// encoder.addFrame(ctx);
+
+	// // blue rectangle
+	// ctx.fillStyle = '#0000ff';
+	// ctx.fillRect(0, 0, 320, 240);
+	// encoder.addFrame(ctx);
+
+	// encoder.finish();
 } init()
--- readme.md
+++ readme.md
@@ -3,15 +3,16 @@
 ### NPM Install 
 >> npm install -g burn-contrib-banner --save-dev
 
-### Basic Use
+### Basic Usage
 go to the root work directory and run below
->> burn
+`>> burn`
 
-### Options 
-- durations frames of banners (milisecond)
-- folders banners (array separate ",")
-- turn banner into zip file (boolean)
-- select element banner turn into image_backup (selector DOM)
+### Parameters 
+- waiting for the end frames of banners (milisecond) `--delay` / `-d`
+- folders banners (array separate ",") `--folder` / `-f`
+- turn on zip feature (boolean) `--zip` / `-z`
+- trun on capture feature (boolean) `--zip` / `-z`
+- selector element to be burn (selector DOM) `--selector` / `-s`
 
-For example: 
->> burn duration folder1,folder2,folder3 false #container
+### Example Parameters Usage: 
+`>> burn -d 10000 -f "drive/folder_target" -z true -c false -s #container`
